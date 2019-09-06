cd67ad040429a91f2aa9612060f7dcf04ecaadb4
mibreit-photo <Michael.Breitung@mibreit-photo.com>
2019-09-06 08:26:07 +0200

    major: fixed scale modes expand and none -> flexbox was interfering with those modes, so now they are manually calculated while fitaspect still uses css calculations

--- src/components/imageWrapper.js
+++ src/components/imageWrapper.js
@@ -11,18 +11,26 @@ const IMAGE_LOADING = "IMAGE_LOADING";
 const IMAGE_LOADED = "IMAGE_LOADED";
 
 export default class ImageWrapper {
-  constructor(image) {
+  constructor(image, limitMaxSize) {
     this._image = image;
     this._originalWidth = parseInt(this._image.getAttribute("width"));
     this._originalHeight = parseInt(this._image.getAttribute("height"));
-    this._title = "";
-    this._state = (!this._image.hasAttribute("data-src")) ? IMAGE_LOADED : IMAGE_INACTIVE;
+    if (limitMaxSize) {
+      $(this._image).css({
+        maxWidth: `${this._originalWidth}px`,
+        maxHeight: `${this._originalHeight}px`
+      });
+    }
+    this._onLoadCallbackInternal = undefined;
+    this._state = !this._image.hasAttribute("data-src") ?
+      IMAGE_LOADED :
+      IMAGE_INACTIVE;
 
-    // center image
-    $(this._image).wrap("<div class=\"mibreit-center-box\"></div>");
+    // wrapper flexbox for image centering
+    $(this._image).wrap('<div class="mibreit-center-box"></div>');
 
     // disable drag
-    $(this._image).on('dragstart', function () {
+    $(this._image).on("dragstart", function () {
       return false;
     });
 
@@ -31,6 +39,7 @@ export default class ImageWrapper {
       return false;
     });
 
+    // retrieve title
     if (this._image.hasAttribute(globals.TITLE_ATTRIBUTE)) {
       // we do this to ensure that title will not show up on hover
       const title = this._image.getAttribute(globals.TITLE_ATTRIBUTE);
@@ -41,8 +50,8 @@ export default class ImageWrapper {
     this._title = this._image.getAttribute(globals.DATA_TITLE_ATTRIBUTE);
   }
 
-  /**     
-   * @return {Promise} Promise that will resolve once the image is loaded 
+  /**
+   * @return {Promise} Promise that will resolve once the image is loaded
    *         and reject, if image was already loaded (with true) or is currently loading (with false)
    */
   loadImage() {
@@ -51,8 +60,13 @@ export default class ImageWrapper {
         this._image.onload = () => {
           this._image.removeAttribute(globals.DATA_SRC_ATTRIBUTE);
           this._state = IMAGE_LOADED;
+
+          if (this._onLoadCallbackInternal) {
+            this._onLoadCallbackInternal();
+          }
+
           resolve();
-        }
+        };
         this._state = IMAGE_LOADING;
 
         this._image.setAttribute(
@@ -76,69 +90,114 @@ export default class ImageWrapper {
   applyScaleMode(containerWidth, containerHeight, scaleMode) {
     switch (scaleMode) {
       case globals.SCALE_MODE_STRETCH:
-        $(this._image).css({
-          width: "100%",
-          height: "100%"
-        });
+        this._applyStretch();
         break;
-      case globals.SCALE_MODE_EXPAND: {
-        const aspect = this._originalWidth / this._originalHeight;
-        if (containerWidth / containerHeight > aspect) {
-          // fit based on width
-          $(this._image).css({
-            width: "100%",
-            height: "auto"
-          });
-        } else {
-          // fit based on height
-          $(this._image).css({
-            width: "auto",
-            height: "100%"
-          });
-        }
-      }
-      break;
-    case globals.SCALE_MODE_FITASPECT: {
-      const aspect = this._originalWidth / this._originalHeight;
-      if (containerWidth / containerHeight > aspect) {
-        // fit based on height        
-        if (containerHeight <= this._originalHeight) {
-          $(this._image).css({
-            width: "auto",
-            height: "100%"
-          });
-        } else {
-          $(this._image).css({
-            width: "auto",
-            height: "auto"
-          });
-        }
-      } else {
-        // fit based on width     
-        if (containerWidth <= this._originalWidth) {
-          $(this._image).css({
-            width: "100%",
-            height: "auto"
-          });
-        } else {
-          $(this._image).css({
-            width: "auto",
-            height: "auto"
-          });
-        }
-      }
-
+      case globals.SCALE_MODE_EXPAND:
+        this._applyExpand(containerWidth, containerHeight);
+        break;
+      case globals.SCALE_MODE_FITASPECT:
+        this._applyFitAspect(containerWidth, containerHeight);
+        break;
+      case globals.SCALE_MODE_NONE:
+      default:
+        this._applyNone(containerWidth, containerHeight);
     }
-    break;
-    case globals.SCALE_MODE_NONE:
-    default:
+  }
+
+  _applyStretch() {
+    $(this._image).css({
+      width: "100%",
+      height: "100%"
+    });
+  }
+
+  _applyExpand(containerWidth, containerHeight) {
+    const aspect = this._originalWidth / this._originalHeight;
+
+    if (containerWidth / containerHeight > aspect) {
+      // fit based on width
       $(this._image).css({
-        width: "auto",
-        height: "auto"
+        width: `${containerWidth}px`,
+        height: `${containerWidth / aspect}px`
+      });
+    } else {
+      // fit based on height
+      $(this._image).css({
+        width: `${containerHeight * aspect}px`,
+        height: `${containerHeight}px`
       });
     }
 
-    return this._image;
+    this._centerImage(containerWidth, containerHeight);
+  }
+
+  _applyFitAspect(containerWidth, containerHeight) {
+    const aspect = this._originalWidth / this._originalHeight;
+    if (containerWidth / containerHeight > aspect) {
+      // fit based on height
+      if (containerHeight <= this._originalHeight) {
+        $(this._image).css({
+          width: "auto",
+          height: "100%"
+        });
+      } else {
+        $(this._image).css({
+          width: "auto",
+          height: "auto"
+        });
+      }
+    } else {
+      // fit based on width
+      if (containerWidth <= this._originalWidth) {
+        $(this._image).css({
+          width: "100%",
+          height: "auto"
+        });
+      } else {
+        $(this._image).css({
+          width: "auto",
+          height: "auto"
+        });
+      }
+    }
+    this._resetImagePosition();
+  }
+
+  _applyNone(containerWidth, containerHeight) {
+    $(this._image).css({
+      width: `${this._originalWidth}px`,
+      height: `${this._originalHeight}px`
+    });
+    this._centerImage(containerWidth, containerHeight);
+  }
+
+  _resetImagePosition() {
+    $(this._image).css({
+      marginLeft: "auto"
+    });
+    $(this._image).css({
+      marginTop: "auto"
+    });
+  }
+
+  _centerImage(containerWidth, containerHeight) {
+    if (this._state != IMAGE_LOADED) {
+      // until image is loaded, we cannot access the css width and height properly
+      this._onLoadCallbackInternal = () => {
+        this._centerImage(containerWidth, containerHeight);
+      };
+    } else {
+      const width = $(this._image).width();
+      const height = $(this._image).height();
+      const x = (width + containerWidth) / 2 - width;
+      const y = (height + containerHeight) / 2 - height;
+      $(this._image).css({
+        marginLeft: x
+      });
+      $(this._image).css({
+        marginTop: y
+      });
+    }
   }
 
   // private helpers
